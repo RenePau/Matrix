9f5b373f16983e109a6def31f4f35cfe17b837e5
RenePau <rene.pauls@hetnet.nl>
2017-12-02 14:38:17 +0100

    jan commentaar

--- src/app/data/dataservice.spec.ts
+++ src/app/data/dataservice.spec.ts
@@ -51,7 +51,8 @@ describe('DataService Injector', () => {
     let service = new DataService('true');
     let testresult = service.getTestNumber();
 
-    //it('dataservice getTestNumber', () => expect(testresult ).toBe(42)); //Success
-    it('dataservice getTestNumber', () => expect(testresult ).toBe(41)); //Failed
+    // comment: koeman wacht op annbieding knvb
+    it('dataservice getTestNumber', () => expect(testresult ).toBe(42)); //Success
+    //it('dataservice getTestNumber', () => expect(testresult ).toBe(41)); //Failed
 
   });
